ee0655cf4185079ff487e84496d19b75d52dcd80
Lynn Y. Hales <halesly@1038.org>
2026-09-23 19:22:39 -0400

    hit

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main{
+    public static void  main(String[] args){
+        System.out.println("1038");
+        System.out.println("Lee Hales");
+        System.out.println("Programming");
+
+        System.out.println("=== ROBOT STATUS ===");
+        System.out.println("Drive: enabled");
+        System.out.println("Intake: ready");
+        System.out.println("Shooter: idle");
+        System.out.println("====================");
+        
+        System.out.println("Autonomous mode:");
+        System.out.print(" starting...");
+
+
+
+    }
+
+}
